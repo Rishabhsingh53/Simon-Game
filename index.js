e6bd73370e2c07da34afe45932631366b3c2533c
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-21 21:23:06 +0530

    getting user clicked id

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ colors = ["red","blue","green","yellow"];
 gamepattern = []
 userClickedPattern = []
 
+$(".btn").click(function () {
+    var userClick = this.getAttribute("id")
+    console.log(userClick);
+})
+
 function nextSeq() {
     var randomNum = Math.floor(Math.random() * 4) 
     var randomChoosenColor = colors[randomNum]
     gamepattern.push(randomChoosenColor)
-    var x = document.querySelector("#"+randomChoosenColor)
     $("#" + randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     // not working right now 
-    var sound = new Audio("sounds/" + randomChoosenColor + ".mp3");
-    sound.play()
+    // var sound = new Audio("sounds/" + randomChoosenColor + ".mp3");
+    // sound.play()
 }
 nextSeq()
